16c9e552e53e294766ba378f36accf45cbae5cd2
AadarshVerma <aadarshverma36@gmail.com>
2021-05-05 12:48:22 +0530

    before azure deploy

--- blog_clone/settings.py
+++ blog_clone/settings.py
@@ -115,7 +115,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR,'static')]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
@@ -124,3 +124,4 @@ LOGIN_REDIRECT_URL = 'home'
 LOGOUT_REDIRECT_URL = 'home'
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
+# DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
